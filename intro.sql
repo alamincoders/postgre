5a2564ea47cd6b356418bb7e20991cdaab73f4ae
alamincoders <developeralamin03@gmail.com>
2023-08-29 12:32:39 +0600

    introduction to sql with postgresql

---
+++ intro.sql
@@ -0,0 +1,34 @@
+-- ==================================
+-- ======= Database Operation =======
+-- ==================================
+
+
+-- Active: 1693286877591@@127.0.0.1@5432@db_test
+-- Create DATABASE
+-- CREATE  DATABASE test_db
+
+-- Delete DATABASE
+-- DROP DATABASE just_database
+
+-- Alter DATABASE
+-- ALTER DATABASE test_db RENAME TO db_test
+
+-- ==================================
+-- ========= Table Operation ========
+-- ==================================
+
+-- create a TABLE
+
+-- CREATE TABLE student (
+--       st_id INT,
+--       f_name VARCHAR(50),
+--       l_name VARCHAR(50),
+--       st_cgp_a NUMERIC(1,2)
+-- )
+
+
+-- update table
+-- ALTER TABLE student RENAME TO learners;
+
+-- DELETE TABLE
+-- DROP TABLE learners;
