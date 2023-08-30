6ca4ff6f72101b1aab5cb1ad820db346c6d8e594
alamincoders <developeralamin03@gmail.com>
2023-08-30 22:29:35 +0600

    insertion

--- intro.sql
+++ intro.sql
@@ -6,13 +6,13 @@
 
 -- Active: 1693286877591@@127.0.0.1@5432@db_test
 -- Create DATABASE
--- CREATE  DATABASE test_db
+CREATE  DATABASE test_db
 
 -- Delete DATABASE
--- DROP DATABASE just_database
+DROP DATABASE just_database
 
 -- Alter DATABASE
--- ALTER DATABASE test_db RENAME TO db_test
+ALTER DATABASE test_db RENAME TO db_test
 
 -- ==================================
 -- ========= Table Operation ========
@@ -20,19 +20,19 @@
 
 -- create a TABLE
 
--- CREATE TABLE student (
---       st_id INT,
---       f_name VARCHAR(50),
---       l_name VARCHAR(50),
---       st_cgp_a NUMERIC(1,2)
--- )
+CREATE TABLE student (
+      st_id INT,
+      f_name VARCHAR(50),
+      l_name VARCHAR(50),
+      st_cgp_a NUMERIC(1,2)
+)
 
 
 -- update table
--- ALTER TABLE student RENAME TO learners;
+ALTER TABLE student RENAME TO learners;
 
 -- DELETE TABLE
--- DROP TABLE learners;
+DROP TABLE learners;
 
 
 -- ==================================
@@ -40,9 +40,21 @@
 -- ==================================
 
 
-CREATE TABLE "user" (
+CREATE TABLE user2 (
       user_id INT PRIMARY KEY,
       username VARCHAR(150) UNIQUE NOT NULL,
       email VARCHAR(150) UNIQUE NOT NULL,
       age INT DEFAULT 18
 )
+
+ALTER TABLE db_test RENAME TO user1;
+
+
+
+
+INSERT INTO user2 (user_id, username, email, age) VALUES ( 3, 'moriom', 'moriom@gmail.com', 21)
+
+SELECT * FROM user2;
+
+
+
